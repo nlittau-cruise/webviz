4547442760abb94d565bf2a4559cc5a4c964ebdc
David Winegar <david.winegar@getcruise.com>
2019-08-20 14:08:30 -0700

    Fix worldview stories/screenshot testing
    * Don't run screenshot tests on tests that have movement
    * Fix stories for worldview not showing up in storybook
    * Fix detection of running screenshot tests
    * Fix non-deterministic MouseEvents story

--- stories/inScreenshotTests.js
+++ stories/inScreenshotTests.js
@@ -5,5 +5,6 @@
 //  You may not use this file except in compliance with the License.
 
 export default function inScreenshotTests() {
-  return navigator.userAgent.includes("HeadlessChrome");
+  // screenshot tests are not always in a headless Chrome, so need to check for "selectedKind" in the URL as well.
+  return navigator.userAgent.includes("HeadlessChrome") || window.location.search.includes("selectedKind");
 }
